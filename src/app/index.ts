bb4f5a7566978caefea92d9d916f5b71548692fc
Diego Ramos <diegorbrito9@gmail.com>
2024-02-26 07:55:37 -0300

    chore: instalando pacotes e adicionando middlewares

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,9 +3,15 @@ import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from '@/utils/errorMiddleware';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
+app.use(express.json()); // enable json use
+app.use(express.urlencoded({ extended: true })); // enable www-form-urlencoded
 app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
 
 // error handling
 app.use(errorMiddleware);

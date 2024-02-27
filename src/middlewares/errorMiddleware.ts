7f31cbab27598fd2a775e28681b13caf26bd10de
Diego Ramos <diegorbrito9@gmail.com>
2024-02-27 20:24:00 -0300

    feat: adicionando tratamento de erro caso seja instaciado um erro das validações de busca por termo

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { Exception } from '@/utils/exception';
+import { ZodError } from 'zod';
 
 export function errorMiddleware(
   error: Error,
@@ -7,6 +8,12 @@ export function errorMiddleware(
   res: Response,
   _next: NextFunction
 ) {
+  if (error instanceof ZodError) {
+    const formatMessage = error.issues;
+
+    return res.status(400).json(formatMessage);
+  }
+
   if (error instanceof Exception) {
     return res.status(error.statusCode).json({ message: error.message });
   }
